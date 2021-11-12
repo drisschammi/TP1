054d5a895e3cdadafc95c9c6ec402d710810d536
driss <driss@DESKTOP-09TQJ7F>
2021-11-12 11:53:42 +0100

    Add project files.

---
+++ TP1/Table.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP1
+{
+    class Table
+    {
+        int x;
+        int lenght;
+        int donnees;
+
+        //Constructeur
+        public Table(int taille, int element)
+        {
+            this.lenght = taille;
+            this.donnees = element;
+        }
+        //Insertion
+        public void remplirtable(int element_ajouter, int new_position)
+        {
+            ajouterelement(element_ajouter, new_position);
+        }
+        public void ajouterelement(int element, int new_position)
+        {
+            int[] Tablea = new int[0];
+
+            for (int i = 0; i < Tablea.Length; i++)
+            {
+                Tablea[i] = Tablea[Tablea.Length + 1];
+
+            }
+        }
+        //Suppression
+        public void supprimerelement(int taille)
+        {
+            int[] Tablea = new int[taille];
+            for (int i = 0; i < Tablea.Length; i++)
+            {
+                Tablea[i] = Tablea[Tablea.Length - 1];
+            }
+        }
+
+    }
+}
+
---
+++ TP1/¨Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+namespace TP1
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        { 
+        }
+    }
+
+}
